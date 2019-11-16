baee575565f097aa196caf4c443c7de415c464d2
jacknpainter <jackpainter2@gmail.com>
2019-11-16 17:01:20 +0000

    Added Printing of Weather/News Info and Keys for Other Motorsports

--- MotorsportApp.py
+++ MotorsportApp.py
@@ -1,12 +1,24 @@
 import requests
 import json
-import pprint
+from pprint import pprint
+from datetime import datetime
 
-f1_api = "http://ergast.com/api/f1"
+f1_api = "http://ergast.com/api/f1/"
 news_url = 'https://newsapi.org/v2/everything?'
 news_key = 'c78ac9726f714b79b3798843898a7400'
+#NEW KEYS
+f1_key = 'ddpgefjjw8jpvsxtzzrar4tg'
+fe_key = 'tghwbk5bbq9xdhh4ffc6nm2q'
+nascar_key = 'b9yp48fazmd4j7dsj3z6fkjr'
+indy_key = 'rantz54umfy25grmk89vt7r5'
+motogp_key = '6hu6q2j774j8k7nvpkntunq6'
 
-# Valid Options: 
+def getNascarRace(series = 'mc', year='2019'):
+	url = "http://api.sportradar.us/nascar-t3/{}/{}/races/schedule.json?api_key={}".format(series,year,nascar_key)
+	result = requests.get(url)
+	return result
+
+# Valid Options:
 # year: see getF1Season options
 # race: any valid 1 or 2 digit race number during the selected season
 def getF1Race(year="current", race="next"):
@@ -27,12 +39,12 @@ def getWeather(city='London'):
 	result = requests.get(key)
 	return result
 
-def getNews(url, pageSize=20,apiKey):
+def getNews(url="https://newsapi.org/v2/everything?",pageSize=20,apiKey="c78ac9726f714b79b3798843898a7400"):
 	# Specify the query and number of returns
 	parameters = {
     		'q': 'F1', # query phrase
     		'pageSize': 20,  # maximum is 100
-    		'apiKey': secret # your own API key
+    		'apiKey': apiKey # your own API key
 	}
 
 	# Make the request
@@ -40,25 +52,41 @@ def getNews(url, pageSize=20,apiKey):
 
 	# Convert to JSON
 	#response_json = response.json()
-	
+
 	return response
-    
+
 def menu():
 	print("Please select which motorsport you would like:")
-	msport = input("Choose from:\nF1)
+	msport = input("Choose from:\n> F1\n> NASCAR\n\n> ")
+	msport.upper()
 
-	motorsports = ['F1', 'f1']
+	motorsports = ['F1', 'NASCAR']
+	date = datetime.today().strftime('%Y-%m-%d')
 
 	while msport not in motorsports:
-	    print("please choose a valid option")
-	    msport = input()
-
-	print("Which year?")
-	year = input()
-
-	print("Which race would you like?")
-	race = input()
-	
-	
+	    print("Please choose a valid option")
+	    msport = input().upper()
 
 
+	if msport == 'F1':
+		f1_json = getF1Race().json()
+		race = f1_json['MRData']['RaceTable']['Races'][0]
+		circuit = race['Circuit']
+		location = circuit['Location']
+
+		print("Location: {}\nCircuit: {}".format(location['country'],circuit['circuitName']))
+		weather = getWeather(location['locality']).json()
+		news_json = getNews().json()
+		headlines = []
+		for i in news_json['articles']:
+		    headlines.append(i['title'])
+
+		pprint(weather)
+		pprint(headlines)
+	elif msport == 'NASCAR':
+		# Needs Completing
+		nascar_json = getNascarRace().json()
+		#pprint(nascar_json)
+
+
+menu()
